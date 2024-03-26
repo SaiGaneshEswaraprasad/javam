23dd50092fcd9680fef24c8c51e04558096d7215
SaiGaneshEswaraprasad <113225851+SaiGaneshEswaraprasad@users.noreply.github.com>
2024-03-26 16:48:16 +0000

    done

--- M4Q8.java
+++ M4Q8.java
@@ -5,43 +5,56 @@
 // Interface in the package Quad1
 package Quad1;
 
-public interface QuadraticEquation {
-    void findRoots(double a, double b, double c);
+public interface QuadraticEquation 
+{
+	
+	void findRoots(double a, double b, double c);
+	
 }
+
 
 ______________________
 
 // Implementation of the interface in the package Quad2
 package Quad2;
+
 import Quad1.QuadraticEquation;
 
-public class QuadraticEquationImpl implements QuadraticEquation {
-    @Override
-    public void findRoots(double a, double b, double c) {
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant > 0) {
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            System.out.println("Root 1: " + root1);
-            System.out.println("Root 2: " + root2);
-        } else if (discriminant == 0) {
-            double root = -b / (2 * a);
-            System.out.println("Root: " + root);
-        } else {
-            System.out.println("The equation has no real roots.");
-        }
-    }
+class QuadraticSolution implements QuadraticEquation
+{
+	
+	    @Override
+	    public void findRoots(double a, double b, double c) 
+	    {
+	        double discriminant = b * b - 4 * a * c;
+	        double root1, root2;
+
+	        if (discriminant > 0) 
+	        {
+	            root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+	            root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+	            System.out.println("Roots: " + root1 + ", " + root2);
+	        } 
+	        else if (discriminant == 0)
+	        {
+	            root1 = -b / (2 * a);
+	            System.out.println("Root: " + root1);
+	        } 
+	        else 
+	        {
+	            double realPart = -b / (2 * a);
+	            double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
+	            System.out.println("Roots: " + realPart + " + " + imaginaryPart + "i, " + realPart + " - " + imaginaryPart + "i");
+	        }
+	    }
 }
 
-______________________
 
-// Main class to use the interface and implementation
-import Quad1.QuadraticEquation;
-import Quad2.QuadraticEquationImpl;
-
-public class Main {
-    public static void main(String[] args) {
-        QuadraticEquation equation = new QuadraticEquationImpl();
-        equation.findRoots(1, -3, 2); // Example quadratic equation: x^2 - 3x + 2 = 0
+public class QuadraticProcedure
+{
+    public static void main(String[] args)
+    {
+    	QuadraticSolution solver = new QuadraticSolution();
+        solver.findRoots(1, -3, 2); // Quadratic equation: x^2 - 3x + 2 = 0
     }
 }
